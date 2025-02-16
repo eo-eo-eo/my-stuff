439387d1d9196fc911fd70d47ba9ab763e1dfbdd
eo-eo-eo <eduardosipov13@outlook.com>
2025-02-16 05:05:05 +0000

    Create TLALLINONE.lua

---
+++ TLALLINONE.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eo-eo-eo/my-stuff/refs/heads/main/UPGRADE.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eo-eo-eo/my-stuff/refs/heads/main/TLPLACER"))()
